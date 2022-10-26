70996216c82676af3e9b85809137dda3a8c33798
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-10-26 14:27:56 +0100

    added navbar

---
+++ Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container p-4 h-14 w-full bg-black text-white'>
+      <ul className='flex justify-center items-center gap-4 text-xl'>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+
+        <li>
+          <Link href="/">Characters</Link>
+        </li>
+
+        <li>
+          <Link href="/">Episodes</Link>
+        </li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
